ebd6922136d40319b5f429dd655f97b2babeb62e
sakshi <sakshi9717021957@gmail.com>
2022-06-30 12:37:37 +0530

    first commit

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class app {
+
+public class app {
+    private final String message = "Hello World!";
+
+    public app() {}
+
+    public static void main(String[] args) {
+        System.out.println(new app().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
 }
--- src/test/java/apptest.java
+++ src/test/java/apptest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class apptest {
-}
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+public class apptest {
+
+        /**
+         * Unit test for simple App.
+         */
+
+
+        private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+        @Before
+        public void setUpStreams() {
+            System.setOut(new PrintStream(outContent));
+        }
+
+        @Test
+        public void testAppConstructor() {
+            try {
+                new app();
+            } catch (Exception e) {
+                fail("Construction failed.");
+            }
+        }
+
+        @Test
+        public void testAppMain()
+        {
+            app.main(null);
+            try {
+                assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            } catch (AssertionError e) {
+                fail("\"message\" is not \"Hello World!\"");
+            }
+        }
+
+        @After
+        public void cleanUpStreams() {
+            System.setOut(null);
+        }
+
+    }
+
+
